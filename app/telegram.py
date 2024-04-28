3ad72eb782f2b8e68d17bbf9d8e49027862c67df
Kingunok <114995588+Kingunok@users.noreply.github.com>
2024-04-28 13:04:46 +0530

    Update telegram.py

--- app/telegram.py
+++ app/telegram.py
@@ -2,49 +2,74 @@ import math
 import logging
 import asyncio
 
-from telethon import TelegramClient, utils
-from telethon.sessions import StringSession
+from pyrogram import Client
+from .util import TokenParser
+from .botCode import multi_clients, work_loads, bot
+from .config import workers,multi_clients,bot_token,api_hash,api_id,sleep_threshold 
 
-
-class Client(TelegramClient):
-    def __init__(self, session_string: str, *args, **kwargs):
-        super().__init__(StringSession(session_string), *args, **kwargs)
-        self.log = logging.getLogger(__name__)
+class CustomClient(Client):
+    async def initialize_clients(self):
+        multi_clients[0] = bot
+        work_loads[0] = 0
+        all_tokens = TokenParser().parse_from_env()
+        if not all_tokens:
+            print("No additional clients found, using default client")
+            return
+        
+        async def start_client(client_id, token):
+            try:
+                print(f"Starting - Client {client_id}")
+                if client_id == len(all_tokens):
+                    await asyncio.sleep(2)
+                    print("This will take some time, please wait...")
+                client = await Client(
+                    name=":memory:",
+                    api_id=api_id,
+                    api_hash=api_hash,
+                    bot_token=token,
+                    sleep_threshold=sleep_threshold,
+                    no_updates=True,
+                ).start()
+                work_loads[client_id] = 0
+                return client_id, client
+            except Exception:
+                logging.error(f"Failed starting Client - {client_id} Error:", exc_info=True)
+        
+        clients = await asyncio.gather(*[start_client(i, token) for i, token in all_tokens.items()])
+        multi_clients.update(dict(clients))
+        if len(multi_clients) != 1:
+            multi_clients = True
+            print("Multi-Client Mode Enabled")
+        else:
+            print("No additional clients were initialized, using default client")
 
     async def download(self, file, file_size, offset, limit):
-        part_size = utils.get_appropriated_part_size(file_size) * 1024
+        part_size = 512 * 1024
         first_part_cut = offset % part_size
         first_part = math.floor(offset / part_size)
         last_part_cut = part_size - (limit % part_size)
         last_part = math.ceil(limit / part_size)
         part_count = math.ceil(file_size / part_size)
         part = first_part
-        self.log.debug(
-            f"""Request Details
-              part_size(bytes) = {part_size},
-              first_part = {first_part}, cut = {first_part_cut}(length={part_size-first_part_cut}),
-              last_part = {last_part}, cut = {last_part_cut}(length={last_part_cut}),
-              parts_count = {part_count}
-            """
-        )
         try:
-            async for chunk in self.iter_download(
-                file, offset=first_part * part_size, request_size=part_size
-            ):
-                self.log.debug(f"Part {part}/{last_part} (total {part_count}) served!")
+            while True:
+                chunk = await self.download_media(
+                    file,
+                    offset=first_part * part_size,
+                    file_size=part_size,
+                    part_size=part_size,
+                    progress=None
+                )
                 if part == first_part:
                     yield chunk[first_part_cut:]
                 elif part == last_part:
                     yield chunk[:last_part_cut]
-                    break
                 else:
                     yield chunk
-
+                self.log.debug(f"Part {part}/{last_part} (total {part_count}) served!")
                 part += 1
-
-            self.log.debug("serving finished")
-        except (GeneratorExit, StopAsyncIteration, asyncio.CancelledError):
-            self.log.debug("file serve interrupted")
-            raise
-        except Exception:
-            self.log.debug("file serve errored", exc_info=True)
+        except FloodWait as e:
+            self.log.warning(f"Flood wait: {e}")
+            await asyncio.sleep(e.x)
+        except Exception as e:
+            self.log.error(f"An error occurred: {e}", exc_info=True)
